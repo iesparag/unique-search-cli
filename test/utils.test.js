859a345c1acaccf10386088a5a2fc57ffa5b336f
iesparag <iesparagjain@gmail.com>
2026-07-06 14:35:44 +0530

    feat: Setup project structure and dependencies (test/utils.test.js)

---
+++ test/utils.test.js
@@ -0,0 +1,10 @@
+// test/utils.test.js
+// Skeleton for utils module tests
+
+import assert from 'node:assert/strict';
+import test from 'node:test';
+
+test('utils module loads', () => {
+  // Placeholder test for utils.js
+  assert.ok(true);
+});
